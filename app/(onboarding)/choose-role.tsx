9fa3e0c017975341d8ce5fb5efcd48691c9cd6fb
Zoe <zoe@berthengineering.com>
2025-07-11 09:02:50 +0000

    Updated onbaording screen

--- app/(onboarding)/choose-role.tsx
+++ app/(onboarding)/choose-role.tsx
@@ -1,25 +1,37 @@
 import { View, Text, TouchableOpacity } from "react-native";
 import { useRouter } from "expo-router";
+import { Image } from "react-native";
 
 export default function ChooseRole() {
   const router = useRouter();
 
   return (
-    <View className="flex-1 items-center justify-center bg-white p-4">
-      <Text className="text-xl font-bold mb-4">Who are you?</Text>
+    <View className="flex-1 items-center justify-center bg-[#5d198a] p-4">
+      <Image
+        source={require('@/assets/images/anansesem-logo.png')}
+        className="w-[200px] h-[200px] mb-6"
+        resizeMode="contain"
+      />
+      <Text>Anansesem</Text>
+      <Text className="text-md mb-4 text-white font-poppins text-center">Only parents can set up an Anansesem account</Text>
 
       <TouchableOpacity
-        // onPress={() => router.push("/(onboarding)/verify-age")}
-        className="bg-blue-500 rounded-full px-6 py-3 mb-4"
+        onPress={() => router.push("/(onboarding)/verify-age")}
+        className="bg-[#D0EE30] rounded-xl px-6 py-3 w-full mb-4"
       >
-        <Text className="text-white font-semibold">I'm a Parent</Text>
+        <Text className="text-[#5d198a] text-center font-semibold font-poppins">I am a PARENT</Text>
       </TouchableOpacity>
 
       <TouchableOpacity
         onPress={() => router.push("/(tabs)")}
-        className="bg-green-500 rounded-full px-6 py-3"
+        className="bg-[#D0EE30] rounded-xl px-6 py-3 w-full"
       >
-        <Text className="text-white font-semibold">I'm a Child</Text>
+        <Text className="text-[#5d198a] text-center font-semibold font-poppins">I am a KID</Text>
+      </TouchableOpacity>
+
+
+      <TouchableOpacity className="mt-20">
+        <Text className="text-white font-poppins underline underline-offset-[5px]">Learn more about Anansesem accounts</Text>
       </TouchableOpacity>
     </View>
   );
--- app/(onboarding)/index.tsx
+++ app/(onboarding)/index.tsx
@@ -1,31 +1,38 @@
 import { View, Text, TouchableOpacity } from 'react-native';
-import { Link, useRouter } from 'expo-router';
-import { Stack } from 'expo-router';
+import { useRouter } from 'expo-router';
 import "global.css";
+import {
+  Image
 
+} from 'react-native';
 export const unstable_settings = {
   initialRouteName: 'welcome',
-};
-
-export const options = {
-  headerShown: false,
 };
 
 export default function WelcomeScreen() {
   const router = useRouter();
 
   return (
-    <View className="flex-1 justify-center items-center px-6" style={{ backgroundColor: '#5d198a' }}>
-      <Text className="font-poppins text-white text-4xl font-bold mb-4">Welcome</Text>
+    <View className="flex-1 justify-center items-center px-6 bg-[#5d198a]">
+
+      <Image
+        source={require('@/assets/images/anansesem-logo-white.png')}
+        className="w-[200px] h-[200px]"
+        resizeMode="contain"
+      />
+      <Text className="mb-6">Anansesem</Text>
+
+
+      <Text className="font-poppins text-[#D0EE30] text-4xl font-bold mb-4">Welcome</Text>
       <Text className="font-poppins text-white text-base text-center mb-8">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque nec justo in quam pulvinar sagittis.
+        Discover a world of imagination and learning! Create your own stories, explore a vast library and learn while you play. Anansesem brings stories to life, tailored just for you!
       </Text>
 
       <TouchableOpacity
         onPress={() => router.replace('/(onboarding)/choose-role')}
-        className="bg-white rounded-full px-6 py-3"
+        className="bg-[#D0EE30] rounded-xl px-6 py-3 w-full"
       >
-        <Text className="font-poppins text-[#5d198a] font-semibold text-base">Next</Text>
+        <Text className="font-poppins text-[#5d198a] font-semibold text-base text-center">Next</Text>
       </TouchableOpacity>
     </View>
   );
