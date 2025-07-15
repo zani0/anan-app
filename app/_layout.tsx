41a7506e8d7f9882c59e21b126ce131e450704d9
Zoe <zoe@berthengineering.com>
2025-07-15 20:55:01 +0000

    Added backend

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -79,7 +79,7 @@ export default function RootLayout() {
           <View className="h-full bg-white" style={{ width: `${progress}%` }} />
         </View>
 
-        <Text className="text-white font-[Poppins_400Regular]">{Math.floor(progress)}%</Text>
+        <Text className="text-white font-poppinsBold">{Math.floor(progress)}%</Text>
       </View>
     );
   }
