af01905ea339397b882d28331e2c50b73aa00d93
zani0 <106967971+zani0@users.noreply.github.com>
2025-07-22 15:46:45 +0000

    Updated stats page

--- app/(tabs)/stats.tsx
+++ app/(tabs)/stats.tsx
@@ -1,4 +1,6 @@
 import Achievements from "@/components/Achievements";
+import CategorySlider from "@/components/CategorySlider";
+import Header from "@/components/Header";
 import Leaderboard from "@/components/Leaderboard";
 import { ScrollView, View } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
@@ -12,6 +14,8 @@ export default function Stats() {
       contentContainerStyle={{ paddingBottom: 100 }}
       className="flex-1 bg-white px-4"
     >
+      <Header />
+      <CategorySlider />
       <Achievements />
       <Leaderboard />
     </ScrollView>
